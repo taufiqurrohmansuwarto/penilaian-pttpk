703037d9a6c978717464b2bc2f9616160c6ee3e1
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-03-09 15:14:27 +0700

    add react-query and next-auth

---
+++ controller/penilaian.controller.js
@@ -0,0 +1,15 @@
+import prisma from "../lib/prisma";
+
+const index = async (req, res) => {};
+const get = async (req, res) => {};
+const create = async (req, res) => {};
+const update = async (req, res) => {};
+const remove = async (req, res) => {};
+
+export default {
+  index,
+  get,
+  create,
+  update,
+  remove,
+};
