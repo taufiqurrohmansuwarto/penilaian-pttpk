6b74c2d1cd8ad1d690ab639acbcde0b0eb120f57
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-03-11 14:44:44 +0700

    last commit

---
+++ controller/penilaian_bulanan.controller.js
@@ -0,0 +1,6 @@
+const detail = async (req, res) => {};
+const index = async (req, res) => {};
+const update = async (req, res) => {};
+const remove = async (req, res) => {};
+
+export default [detail, index, update, remove];
