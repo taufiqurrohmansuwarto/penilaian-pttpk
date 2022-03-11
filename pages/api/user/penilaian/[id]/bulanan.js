076d66f39fb47418dd31cd307d72ddda7f597531
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-03-11 14:48:49 +0700

    last commit

---
+++ pages/api/user/penilaian/[id]/bulanan.js
@@ -0,0 +1,4 @@
+import penilaian_bulananController from "../../../../../controller/penilaian_bulanan.controller";
+import handler from "../../../../../lib/handler";
+
+export default handler.get(penilaian_bulananController.get);
