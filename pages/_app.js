721b60cdc46e3555997e673f15d15f597fd0e92a
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-03-13 01:47:10 +0700

    add axios and react-query

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,15 @@
-import 'semantic-ui-css/semantic.min.css'
-import '../styles/global.css'
+import { useState } from "react";
+import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
+import "semantic-ui-css/semantic.min.css";
+import "../styles/global.css";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps?.dehydrateState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
 import * as React from "react";
+import { Button } from "semantic-ui-react";
 
 export default function Home() {
+  const handleBar = () => {
+    alert("etst");
+  };
+
+  const handleClick = () => {
+    alert("etst");
+  };
+
+  const test = (arr) => {
+    return arr.map((t) => !!t?.x);
+  };
+
   return (
     <div>
       <p>lorem</p>
+      <Button active animated onClick={handleClick}>
+        something
+      </Button>
       <h1>hello world</h1>
     </div>
   );
 }
+
+// karena kita akan
