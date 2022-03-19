3ec2048a959a428195374c641678cffef3dbd501
Iput <taufiqurrohman.suwarto@gmail.com>
2022-03-19 20:43:18 +0700

    add services

---
+++ services/users.service.js
@@ -0,0 +1,17 @@
+//  create penialain tahunana
+export const getPenilaian = () => {};
+export const updatePenilaian = () => {};
+export const hapusPenilaian = () => {};
+export const createPenilaian = () => {};
+
+// crate penilaain bulanan
+export const getPenilaianBulanan = () => {};
+export const updatePenilaianBulanan = () => {};
+export const hapusPenilaianBulanan = () => {};
+export const createPenilaianBulanan = () => {};
+
+// target tahunan
+export const getTargetTahunan = () => {};
+export const updateTargeTahunan = () => {};
+export const hapusTargetTahunan = () => {};
+export const createTargetTahunan = () => {};
