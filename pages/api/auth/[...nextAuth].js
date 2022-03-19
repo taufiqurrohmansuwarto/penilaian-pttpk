a4d5253d99544ce1acbcd179ea8da9bb84dedfba
Iput <taufiqurrohman.suwarto@gmail.com>
2022-03-19 15:37:53 +0700

    add custom auth

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -1,2 +1,159 @@
+import NextAuth from "next-auth/next";
+
 const masterClientId = process.env.MASTER_ID;
 const masterClientSecret = process.env.MASTER_SECRET;
+const masterWellKnown = process.env.MASTER_WELLKNOWN;
+
+const pttpkClientId = process.env.PTTPK_ID;
+const pttpkClientSecret = process.env.PTTPK_SECRET;
+const pttpkWellKnowon = process.env.PTTPK_WELLKNOWN;
+
+const pttpkFasilitatorClientId = process.env.PTTPKFASILITATOR_ID;
+const pttpkFasilitatorClientSecret = process.env.PTTPKFASILITATOR_SECRET;
+const pttpkFasilitatorWellKnown = process.env.PTTPKFASILITATOR_WELLKNOWN;
+
+export default NextAuth({
+  pages: {
+    signIn: "/esign/signin",
+  },
+  providers: [
+    {
+      name: "SIMASTER",
+      id: "master",
+      type: "oauth",
+      wellKnown: masterWellKnown,
+      clientId: masterClientId,
+      clientSecret: masterClientSecret,
+      authorization: {
+        params: {
+          scope,
+          prompt: "login",
+        },
+      },
+      idToken: true,
+      checks: ["pkce", "state"],
+      profile(profile, token) {
+        const currentToken = token.id_token;
+        const { role, group, employee_number } =
+          jsonwebtoken.decode(currentToken);
+
+        const currentUser = {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          employee_number: employee_number || "",
+          role,
+          group,
+        };
+
+        return currentUser;
+      },
+    },
+    {
+      name: "PTTPK",
+      id: "pttpk",
+      type: "oauth",
+      wellKnown: pttpkWellKnowon,
+      clientId: pttpkClientId,
+      clientSecret: pttpkClientSecret,
+      authorization: {
+        params: {
+          scope,
+          prompt: "login",
+        },
+      },
+      idToken: true,
+      checks: ["pkce", "state"],
+      profile(profile, token) {
+        const currentToken = token.id_token;
+        const { role, group, employee_number } =
+          jsonwebtoken.decode(currentToken);
+
+        const currentUser = {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          employee_number: employee_number || "",
+          role,
+          group,
+        };
+
+        return currentUser;
+      },
+    },
+    {
+      name: "PTTPK FASILITATOR",
+      id: "pttpk-fasilitator",
+      type: "oauth",
+      wellKnown: pttpkFasilitatorWellKnown,
+      clientId: pttpkFasilitatorClientId,
+      clientSecret: pttpkFasilitatorClientSecret,
+      authorization: {
+        params: {
+          scope,
+          prompt: "login",
+        },
+      },
+      idToken: true,
+      checks: ["pkce", "state"],
+      profile(profile, token) {
+        const currentToken = token.id_token;
+        const { role, group, employee_number } =
+          jsonwebtoken.decode(currentToken);
+
+        const currentUser = {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          employee_number: employee_number || "",
+          role,
+          group,
+        };
+
+        return currentUser;
+      },
+    },
+  ],
+  callbacks: {
+    redirect: async (url, baseUrl) => {
+      return Promise.resolve(`${url?.baseUrl}${process.env.BASE_PATH}`);
+    },
+    async session({ session, token, user }) {
+      session.accessToken = token.accessToken;
+      session.expires = token?.expires;
+
+      // session.scope = token.scope;
+      session.user.id = token.id;
+      session.user.role = token?.role;
+      session.user.group = token?.group;
+      session.user.employee_number = token?.employee_number;
+
+      const check = Date.now() < new Date(token?.expires * 1000);
+
+      if (check) {
+        return session;
+      }
+    },
+    async jwt({ token, account, isNewUser, profile, user }) {
+      if (account) {
+        token.accessToken = account?.access_token;
+
+        token.expires = profile.exp;
+        token.id = account?.providerAccountId;
+        token.role = profile?.role;
+        token.group = profile?.group;
+        token.employee_number = profile?.employee_number;
+      }
+
+      return token;
+    },
+  },
+  theme: "light",
+  secret: process.env.SECRET,
+  jwt: {
+    secret: process.env.SECRET,
+  },
+});
