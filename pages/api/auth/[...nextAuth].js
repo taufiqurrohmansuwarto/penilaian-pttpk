314ada14ce73d635a1e5c1237323f10461d3f00f
Iput <taufiqurrohman.suwarto@gmail.com>
2022-04-02 07:55:58 +0700

    upsert user when login

--- pages/api/auth/[...nextAuth].js
+++ pages/api/auth/[...nextAuth].js
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth/next";
+import prisma from "../../../lib/prisma";
 
 const jsonwebtoken = require("jsonwebtoken");
 
@@ -18,145 +19,154 @@ const pttpkFasilitatorWellKnown = process.env.PTTPKFASILITATOR_WELLKNOWN;
 const pttpkFasilitatorScope = process.env.PTTPKFASILITATOR_SCOPE;
 
 export default NextAuth({
-  providers: [
-    {
-      name: "SIMASTER",
-      id: "master",
-      type: "oauth",
-      wellKnown: masterWellKnown,
-      clientId: masterClientId,
-      clientSecret: masterClientSecret,
-      authorization: {
-        params: {
-          scope: masterScope,
-          prompt: "login",
+    providers: [
+        {
+            name: "SIMASTER",
+            id: "master",
+            type: "oauth",
+            wellKnown: masterWellKnown,
+            clientId: masterClientId,
+            clientSecret: masterClientSecret,
+            authorization: {
+                params: {
+                    scope: masterScope,
+                    prompt: "login"
+                }
+            },
+            idToken: true,
+            checks: ["pkce", "state"],
+            profile(profile, token) {
+                const currentToken = token.id_token;
+                const { role, group, employee_number } =
+                    jsonwebtoken.decode(currentToken);
+
+                const currentUser = {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    employee_number: employee_number || "",
+                    role,
+                    group
+                };
+
+                return currentUser;
+            }
         },
-      },
-      idToken: true,
-      checks: ["pkce", "state"],
-      profile(profile, token) {
-        const currentToken = token.id_token;
-        const { role, group, employee_number } =
-          jsonwebtoken.decode(currentToken);
+        {
+            name: "PTTPK",
+            id: "pttpk",
+            type: "oauth",
+            wellKnown: pttpkWellKnowon,
+            clientId: pttpkClientId,
+            clientSecret: pttpkClientSecret,
+            authorization: {
+                params: {
+                    scope: pttpkScope,
+                    prompt: "login"
+                }
+            },
+            idToken: true,
+            checks: ["pkce", "state"],
+            profile(profile, token) {
+                const currentToken = token.id_token;
+                const { role, group, employee_number } =
+                    jsonwebtoken.decode(currentToken);
 
-        const currentUser = {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-          employee_number: employee_number || "",
-          role,
-          group,
-        };
+                const currentUser = {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    employee_number: employee_number || "",
+                    role,
+                    group
+                };
 
-        return currentUser;
-      },
-    },
-    {
-      name: "PTTPK",
-      id: "pttpk",
-      type: "oauth",
-      wellKnown: pttpkWellKnowon,
-      clientId: pttpkClientId,
-      clientSecret: pttpkClientSecret,
-      authorization: {
-        params: {
-          scope: pttpkScope,
-          prompt: "login",
+                return currentUser;
+            }
         },
-      },
-      idToken: true,
-      checks: ["pkce", "state"],
-      profile(profile, token) {
-        const currentToken = token.id_token;
-        const { role, group, employee_number } =
-          jsonwebtoken.decode(currentToken);
+        {
+            name: "PTTPK FASILITATOR",
+            id: "pttpk-fasilitator",
+            type: "oauth",
+            wellKnown: pttpkFasilitatorWellKnown,
+            clientId: pttpkFasilitatorClientId,
+            clientSecret: pttpkFasilitatorClientSecret,
+            authorization: {
+                params: {
+                    scope: pttpkFasilitatorScope,
+                    prompt: "login"
+                }
+            },
+            idToken: true,
+            checks: ["pkce", "state"],
+            async profile(profile, token) {
+                const currentToken = token.id_token;
+                const { role, group, employee_number } =
+                    jsonwebtoken.decode(currentToken);
 
-        const currentUser = {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-          employee_number: employee_number || "",
-          role,
-          group,
-        };
+                const currentUser = {
+                    id: profile.sub,
+                    name: profile.name,
+                    email: profile.email,
+                    image: profile.picture,
+                    employee_number: employee_number || "",
+                    role,
+                    group
+                };
 
-        return currentUser;
-      },
-    },
-    {
-      name: "PTTPK FASILITATOR",
-      id: "pttpk-fasilitator",
-      type: "oauth",
-      wellKnown: pttpkFasilitatorWellKnown,
-      clientId: pttpkFasilitatorClientId,
-      clientSecret: pttpkFasilitatorClientSecret,
-      authorization: {
-        params: {
-          scope: pttpkFasilitatorScope,
-          prompt: "login",
+                // upsert the user
+
+                await prisma.users.upsert({
+                    where: {
+                        custom_id: profile?.sub
+                    },
+                    create: {},
+                    update: {}
+                });
+                return currentUser;
+            }
+        }
+    ],
+    callbacks: {
+        redirect: async (url, baseUrl) => {
+            const urlCallback = `${url?.baseUrl}${process.env.BASE_PATH}`;
+            return urlCallback;
         },
-      },
-      idToken: true,
-      checks: ["pkce", "state"],
-      profile(profile, token) {
-        const currentToken = token.id_token;
-        const { role, group, employee_number } =
-          jsonwebtoken.decode(currentToken);
+        async session({ session, token, user }) {
+            session.accessToken = token.accessToken;
+            session.expires = token?.expires;
 
-        const currentUser = {
-          id: profile.sub,
-          name: profile.name,
-          email: profile.email,
-          image: profile.picture,
-          employee_number: employee_number || "",
-          role,
-          group,
-        };
+            // session.scope = token.scope;
+            session.user.id = token.id;
+            session.user.role = token?.role;
+            session.user.group = token?.group;
+            session.user.employee_number = token?.employee_number;
 
-        return currentUser;
-      },
+            const check = Date.now() < new Date(token?.expires * 1000);
+
+            if (check) {
+                return session;
+            }
+        },
+        async jwt({ token, account, isNewUser, profile, user }) {
+            if (account) {
+                token.accessToken = account?.access_token;
+
+                token.expires = profile.exp;
+                token.id = account?.providerAccountId;
+                token.role = profile?.role;
+                token.group = profile?.group;
+                token.employee_number = profile?.employee_number;
+            }
+
+            return token;
+        }
     },
-  ],
-  callbacks: {
-    redirect: async (url, baseUrl) => {
-      const urlCallback = `${url?.baseUrl}${process.env.BASE_PATH}`;
-      return urlCallback;
-    },
-    async session({ session, token, user }) {
-      session.accessToken = token.accessToken;
-      session.expires = token?.expires;
-
-      // session.scope = token.scope;
-      session.user.id = token.id;
-      session.user.role = token?.role;
-      session.user.group = token?.group;
-      session.user.employee_number = token?.employee_number;
-
-      const check = Date.now() < new Date(token?.expires * 1000);
-
-      if (check) {
-        return session;
-      }
-    },
-    async jwt({ token, account, isNewUser, profile, user }) {
-      if (account) {
-        token.accessToken = account?.access_token;
-
-        token.expires = profile.exp;
-        token.id = account?.providerAccountId;
-        token.role = profile?.role;
-        token.group = profile?.group;
-        token.employee_number = profile?.employee_number;
-      }
-
-      return token;
-    },
-  },
-  theme: "light",
-  secret: process.env.SECRET,
-  jwt: {
+    theme: "light",
     secret: process.env.SECRET,
-  },
+    jwt: {
+        secret: process.env.SECRET
+    }
 });
