0c6b8064cd4af3f8c16be2abd84a9d0781f4b7d6
Iput Taufiqurrohman Suwarto <taufiqurrohman.suwarto@gmail.com>
2022-04-21 08:52:07 +0700

    add tugas tambahan controller with crud

--- controller/pekerjaan-tambahan.controller.js
+++ controller/pekerjaan-tambahan.controller.js
@@ -4,28 +4,83 @@ const index = async (req, res) => {
     const { id } = req.query;
     const { customId } = req.user;
     try {
-        const result = await prisma;
-    } catch (error) {}
+        const result = await prisma.tugas_tambahan.findMany({
+            where: {
+                penilaian_id: id,
+                penilaian: {
+                    user_custom_id: customId,
+                    aktif: true
+                }
+            }
+        });
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ code: 400, message: "Internal Server Error" });
+    }
 };
 
 const create = async (req, res) => {
+    // const { id } = req.query;
+    // const { customId } = req.user;
+    const { body } = req;
+
     try {
-    } catch (error) {}
+        await prisma.tugas_tambahan.create({
+            data: body
+        });
+        res.json({ code: 200, message: "success" });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ code: 400, message: "Internal Server Error" });
+    }
 };
 
 const update = async (req, res) => {
+    const { tambahanId } = req.query;
+    const { body } = req;
     try {
-    } catch (error) {}
+        await prisma.tugas_tambahan.update({
+            where: {
+                id: tambahanId
+            },
+            data: body
+        });
+        res.json({ code: 200, message: "success" });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ code: 400, message: "Internal Server Error" });
+    }
 };
 
 const detail = async (req, res) => {
+    const { tambahanId } = req.query;
     try {
-    } catch (error) {}
+        const result = await prisma.tugas_tambahan.findUnique({
+            where: {
+                id: tambahanId
+            }
+        });
+        res.json(result);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ code: 400, message: "Internal Server Error" });
+    }
 };
 
 const remove = async (req, res) => {
+    const { tambahanId } = req.query;
     try {
-    } catch (error) {}
+        await prisma.tugas_tambahan.delete({
+            where: {
+                id: tambahanId
+            }
+        });
+        res.json({ code: 200, message: "success" });
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({ code: 400, message: "Internal Server Error" });
+    }
 };
 
 module.exports = {
